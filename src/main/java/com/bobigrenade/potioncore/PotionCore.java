69ba30d6dee3bd7987050746149180d4880d27cd
RupertJelnikarMrak <jelnikar.mrak.rupert+github@gmail.com>
2024-01-28 22:16:59 +0100

    Moved all registry object to a common class

--- src/main/java/com/bobigrenade/potioncore/PotionCore.java
+++ src/main/java/com/bobigrenade/potioncore/PotionCore.java
@@ -1,20 +1,12 @@
 package com.bobigrenade.potioncore;
 
-import com.bobigrenade.potioncore.effect.ModEffects;
-import com.bobigrenade.potioncore.item.ModCreativeModeTabs;
-import com.bobigrenade.potioncore.item.ModItems;
-import com.bobigrenade.potioncore.potion.ModPotions;
+import com.bobigrenade.potioncore.api.PotionCoreObjects;
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -22,46 +14,19 @@ import org.slf4j.Logger;
 @Mod(PotionCore.MOD_ID)
 public class PotionCore {
 
-  // Define mod id in a common place for everything to reference
-  public static final String MOD_ID = "potioncore";
-  // Directly reference a slf4j logger
-  private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "potioncore";
 
-  public PotionCore() {
-    IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-    LOGGER.info("I brewed all the potions, I want more!");
+    public PotionCore() {
+        LOGGER.info("I brewed all the potions, I want more!");
 
-    ModItems.register(modEventBus);
-    ModCreativeModeTabs.register(modEventBus);
-    ModEffects.register(modEventBus);
-    ModPotions.register(modEventBus);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-    // Register the commonSetup method for mod loading
-    modEventBus.addListener(this::commonSetup);
+        PotionCoreObjects.register(modEventBus);
 
-    // Register ourselves for server and other game events we are interested in
-    MinecraftForge.EVENT_BUS.register(this);
-
-    // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-    ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-  }
-
-  private void commonSetup(final FMLCommonSetupEvent event) {}
-
-  // You can use SubscribeEvent and let the Event Bus discover methods to call
-  @SubscribeEvent
-  public void onServerStarting(ServerStartingEvent event) {}
-
-  // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-  @Mod.EventBusSubscriber(
-    modid = MOD_ID,
-    bus = Mod.EventBusSubscriber.Bus.MOD,
-    value = Dist.CLIENT
-  )
-  public static class ClientModEvents {
-
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {}
-  }
+        MinecraftForge.EVENT_BUS.register(this);
+        
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
 }
--- src/main/java/com/bobigrenade/potioncore/api/PotionCoreObjects.java
+++ src/main/java/com/bobigrenade/potioncore/api/PotionCoreObjects.java
@@ -1,12 +1,28 @@
 package com.bobigrenade.potioncore.api;
 
 import com.bobigrenade.potioncore.PotionCore;
-
+import com.bobigrenade.potioncore.effect.CureEffect;
+import com.bobigrenade.potioncore.effect.FreezeEffect;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+/*
+ * Here are all objects that are registered by this mod, so you can call them from here.
+ */
 
 public class PotionCoreObjects {
 
@@ -17,11 +33,128 @@ public class PotionCoreObjects {
             PotionCore.MOD_ID
         );
 
+        /*
+         * Arrow Damage
+         */
         public static final RegistryObject<Attribute> ARROW_DAMAGE = ATTRIBUTES.register(
             "arrow_damage",
-            () -> new RangedAttribute("generic.arrow_damage", 1.0D, 0.0D, 10.0D)
+            () ->
+                new RangedAttribute(
+                    "potioncore:arrow_damage",
+                    1.0D,
+                    0.0D,
+                    10.0D
+                )
         );
 
+        public static void register(IEventBus eventBus) {
+            ATTRIBUTES.register(eventBus);
+        }
+    }
 
+    public static class MobEffects {
+
+        public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(
+            ForgeRegistries.MOB_EFFECTS,
+            PotionCore.MOD_ID
+        );
+
+        /*
+         * Freeze
+         */
+        public static final RegistryObject<MobEffect> FREEZE = MOB_EFFECTS.register(
+            "freeze",
+            () -> new FreezeEffect(MobEffectCategory.HARMFUL, 3124687)
+        );
+
+        /*
+         * Cure
+         */
+        public static final RegistryObject<MobEffect> CURE = MOB_EFFECTS.register(
+            "cure",
+            () -> new CureEffect(MobEffectCategory.BENEFICIAL, 16777215)
+        );
+
+        public static void register(IEventBus eventBus) {
+            MOB_EFFECTS.register(eventBus);
+        }
+    }
+
+    public static class Potions {
+
+        public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(
+            ForgeRegistries.POTIONS,
+            PotionCore.MOD_ID
+        );
+
+        /*
+         * Freeze potion
+         */
+        public static final RegistryObject<Potion> FREEZE_POTION = POTIONS.register(
+            "freeze_potion",
+            () ->
+                new Potion(
+                    new MobEffectInstance(MobEffects.FREEZE.get(), 200, 0)
+                )
+        );
+
+        /*
+         * Cure potion
+         */
+        public static final RegistryObject<Potion> CURE_POTION = POTIONS.register(
+            "cure_potion",
+            () -> new Potion(new MobEffectInstance(MobEffects.CURE.get(), 0, 0))
+        );
+
+        public static void register(IEventBus eventBus) {
+            POTIONS.register(eventBus);
+        }
+    }
+
+    public static class CreativeModeTabs {
+
+        public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(
+            Registries.CREATIVE_MODE_TAB,
+            PotionCore.MOD_ID
+        );
+
+        public static final RegistryObject<CreativeModeTab> POTIONCORE_TAB = CREATIVE_MODE_TABS.register(
+            "potioncore_tab",
+            () ->
+                CreativeModeTab
+                    .builder()
+                    .icon(() -> getPotionTabIcon())
+                    .title(Component.translatable("creativetab.potioncore"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(
+                            PotionUtils.setPotion(
+                                new ItemStack(Items.POTION),
+                                Potions.CURE_POTION.get()
+                            )
+                        );
+                        pOutput.accept(
+                            PotionUtils.setPotion(
+                                new ItemStack(Items.POTION),
+                                Potions.FREEZE_POTION.get()
+                            )
+                        );
+                    })
+                    .build()
+        );
+
+        public static void register(IEventBus eventBus) {
+            CREATIVE_MODE_TABS.register(eventBus);
+        }
+
+        private static ItemStack getPotionTabIcon() {
+            return PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.CURE_POTION.get());
+        }
+    }
+
+    public static void register(IEventBus eventBus) {
+        Attributes.register(eventBus);
+        MobEffects.register(eventBus);
+        Potions.register(eventBus);
+        CreativeModeTabs.register(eventBus);
     }
 }
